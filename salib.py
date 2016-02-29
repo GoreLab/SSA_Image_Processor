9a6f2a40b78c3519490adc11e4bd0f923c88f01d
Kevin Kreher <kmk279@cornell.edu>
2016-02-29 11:45:02 -0500

    Code as of Feb 29

---
+++ salib.py
@@ -0,0 +1,342 @@
+# salib.py
+# library containing functions for samain.py
+#
+# author:
+# Kevin Kreher
+# KMK279@cornell.edu
+#
+# compatibility python version 2.7.10
+
+import numpy as np
+import cv
+import cv2
+import math
+import glob
+import copy
+
+def imageThreshold(img,threshVal):
+	# converts an image to black a white
+	#
+	# img - is the image to threshold
+	# threshVal - always an integer between 0 and 255 that
+	# sets the threshold algorithm cutoff value
+	#
+	# returns a thresholded BW image imgBW
+
+	maxVal = 255 # full swing B/W
+	x,imgBW = cv2.threshold(img,threshVal,maxVal,cv2.THRESH_BINARY)
+	return imgBW
+
+
+# UNUSED
+def blendImages(img1,img2,opac):
+	# blends two images
+	# transparency % = 1-(opac/100)
+	# dimensions and channels of images must agree
+	#
+	# img1 - image (transparent)
+	# img2 - image (background)
+	# opac - opacity for the image, always an
+	# FLOAT between 0.0 and 100.0 (floats must have .0 at end)
+	#
+	# returns the resulting blended image blendResult
+
+	val1 = opac/100
+	val2 = 1-val1
+	blendResult = cv2.addWeighted(img1,val1,img2,val2,0)
+	return blendResult
+
+
+def rotateImage(src,angl,midpt):
+	# rotates the given image by the given angle
+	# around the given midpoint
+	#
+	# src - is the image to be rotated
+	# angl - the angle to rotate the image by around the midpoint
+	# midpt - midpoint to rotate about
+	#
+	# returns the rotated image imgRot
+
+	rows,cols = src.shape[:2]
+	M = cv2.getRotationMatrix2D((midpt[0],midpt[1]),angl,1)
+	imgRot = cv2.warpAffine(src,M,(cols,rows))
+	return imgRot
+
+
+def findMaxSizeBounds(imgBW,thrVal):
+	# finds the object with the maximimum area
+	# to do this it performs thresholding on a BW image
+	# base on well conditioned parameters and images
+	# this max area object should be the seed
+	#
+	# imgBW - black and white image containing the seed
+	# thrVal - value to use for thresholding (set to 135 default)
+	#
+	# returns the bounding info formatted as a dict structure
+	# 'contourList' is a list containing the contour positions,
+	# and 'indexSeed' is the index position of the largest contour
+
+	# threshold image
+	imageThreshed = imageThreshold(imgBW,thrVal)
+	# end threshold image
+
+	# find contours
+	# create copy of image before passing to findContours
+	imageContour = copy.copy(imageThreshed)
+	contours, hierarchy = cv2.findContours(imageContour,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+	# we need to find the contour with max area, the seed
+	i = 0
+	largest = 0
+	largestIndex = 0
+	while i < len(contours):
+		area = cv2.contourArea(contours[i])
+		if area > largest:
+			largest = area
+			largestIndex = i
+		i += 1
+	# end find contours
+
+	return {'seedIndex':largestIndex, 'contourList':contours}
+
+
+def lensCorrectImages(row,col):
+    # removes lens distortion
+    # algorithm works on all images in working directory
+    # must be preprocessed with Picasa (note: Picasa makes all jpg)
+    #
+    # Sources:
+    # http://opencv-python-tutroals.readthedocs.org
+    # /en/latest/py_tutorials/py_calib3d/py_calibration/py_calibration.html
+    # 
+    # http://docs.opencv.org/2.4/doc/tutorials/calib3d/camera_calibration
+    # /camera_calibration.html
+    # 
+    # Based of the asymmetrical circle pattern found below:
+    # http://docs.opencv.org/2.4/_downloads/acircles_pattern.png
+    #
+    # MUST HAVE >10 CALIBRATION IMAGES IN DIRECTORY /calibration/ as .jpg
+    # MUST HAVE SEED IMAGES IN DIRECTORY /seedImages/ as .jpg
+    #
+    # row - rows of asymmetrical circle pattern to detect (can be smaller than printed)
+    # col - columns of asymmetrical circle pattern to detect (can be smaller than printed)
+    # be sure that image directory locations are correct
+    #
+    # returns 1
+
+    pts_x = row #6
+    pts_y = col #7
+
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((pts_x*pts_y,3), np.float32)
+    objp[:,:2] = np.mgrid[0:pts_y,0:pts_x].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    # calibration image directory, need at least 10
+    images = glob.glob('calibration/*.jpg')
+
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+        # Find the circle centers
+        ret, centers = cv2.findCirclesGrid(gray, (pts_y,pts_x),cv2.CALIB_CB_SYMMETRIC_GRID)
+
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+
+            # unused for circle pattern
+            #corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
+            imgpoints.append(centers)
+
+            # Draw and display the centers
+            img = cv2.drawChessboardCorners(img, (pts_y,pts_x), centers,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(500)
+
+    cv2.destroyAllWindows()
+
+    # calculate camera characteristics
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+
+    # undistort all images in working directory
+    imagesCorr = glob.glob('seeedImages/TopImage*.jpg')
+	# undistort top images only
+
+    for fname in imagesCorr:
+
+    	# debug test
+    	print fname
+
+    	# undistort the specified image
+    	img = cv2.imread(fname)
+    	h,  w = img.shape[:2]
+    	newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+    	dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    	# crop the image
+    	x,y,w,h = roi
+    	dst = dst[y:y+h, x:x+w]
+    	cv2.imshow(fname,dst)
+    	cv2.waitKey(0)
+    	#cv2.imwrite(fname,dst)
+
+    return 1
+
+
+def findLengthWidth(workingImg,thrVal):
+    # find seed contour - at a certain index position in the list of contours found
+    # TODO input and output, error output (all 1)
+
+    seedBounds = findMaxSizeBounds(workingImg,thrVal)
+    largestIndex = seedBounds['seedIndex']
+    contours = seedBounds['contourList']
+
+    # error catching if NO CONTOUR IS FOUND
+    if len(contours) == 0:
+        return {'length':1, 'width':1, 'angle':0,
+            'center':(1,1), 'area':1, 'largestIndex':0, 'contours':[np.array([[[199, 519]]], dtype=np.int32)]}
+
+
+    # create rectangle (rotated) containing seed, and find midpoints
+    bounds = cv2.minAreaRect(contours[largestIndex])
+    # box used for finding midpoints and drawing
+    box = cv2.cv.BoxPoints(bounds) # note cv2.cv.BoxPoints(rect) will be removed in openCV 3
+    #cv2.boxPoints(bounds) - if openCV 3
+    box = np.int0(box)
+    # find midpoints
+    # A = midpoint of points at box[0] and box[1]
+    A = ((box[0][0]+box[1][0])/2 , (box[0][1]+box[1][1])/2)
+    # B = midpoint of box[1] and box[2]
+    B = ((box[1][0]+box[2][0])/2 , (box[1][1]+box[2][1])/2)
+    # C = midpoint of box[2] and box[3]
+    C = ((box[2][0]+box[3][0])/2 , (box[2][1]+box[3][1])/2)
+    # D = midpoint of box[3] and box[0]
+    D = ((box[3][0]+box[0][0])/2 , (box[3][1]+box[0][1])/2)
+    # center = midpoints of A-C line and B-D
+    centerPoint = ((B[0]+D[0])/2 , (A[1]+C[1])/2)
+    # end create rectangle, midpoints
+
+    # calculate geometry - length, width, area
+    areaTop = cv2.contourArea(contours[largestIndex])
+    distance1 = math.sqrt(math.pow(B[0]-D[0],2)+math.pow(B[1]-D[1],2))
+    distance2 = math.sqrt(math.pow(A[0]-C[0],2)+math.pow(A[1]-C[1],2))
+    # assign correct width or length
+    if distance1 > distance2:
+        length = distance1
+        width = distance2
+        rotateAngle = 90+bounds[2]
+    else:
+        length = distance2
+        width = distance1
+        rotateAngle = bounds[2]
+    # end geometry calculation
+    return {'length':length, 'width':width, 'angle':rotateAngle,
+    'center':centerPoint, 'area':areaTop, 'largestIndex':largestIndex, 'contours':contours}
+
+
+def pixelSizeCalibrate(imgBW,thrVal,modelLength,modelWidth):
+    # returns parameters useful for the calibration of pixel distances to
+    # standard unit measurements (does not correct for lens distortion).
+    #
+    # imgBW - input image with a calibration rectangle
+    # thrVal - value to use for thresholding (set to 150 default)
+    # modelLegnth - must be in centimeters, measured length of "model"
+    # modelWidth - must be in centimeters, measured width of "model"
+    #
+    # returns two scaling factors - length, width (in cm/pixel) as a tuple
+
+    imgBW_inverted = cv2.bitwise_not(imgBW)
+    calibImageVariables  = findLengthWidth(imgBW_inverted,thrVal)
+    #calibLepngth = calibImageVariables['length']
+    #calibWidth = calibImageVariables['width']
+    #calibRotateAngle = calibImageVariables['angle']
+    #calibCenterPoint = calibImageVariables['center']
+    #calibArea = calibImageVariables['area']
+    #calibLargestIndex = calibImageVariables['largestIndex'] 
+    #calibContours = calibImageVariables['contours']
+
+    # find known shape
+    lengthModelpix = calibImageVariables['length'] # in pixels - found above
+    widthModelpix = calibImageVariables['width'] # in pixels - found above
+    lengthModelcm = modelLength
+    widthModelcm = modelWidth
+    # pixel scale
+    lengthScaleFactor = lengthModelcm/lengthModelpix
+    widthScaleFactor = widthModelcm/widthModelpix
+
+    # debug output
+    #debugImage = imageScale
+    # draw contour trace
+    #cv2.drawContours(debugImage,scaleVariables['contours'],scaleVariables['largestIndex'],(0,0,255),1)
+    # draw rectangle
+    # create top image rectangle (rotated) containing seed, and find midpoints
+    #bounds = cv2.minAreaRect(scaleVariables['contours'][scaleVariables['largestIndex']])
+    # box used for finding midpoints and drawing
+    #box = cv2.cv.BoxPoints(bounds) # note cv2.cv.BoxPoints(rect) will be removed in openCV 3
+    #        cv2.boxPoints(bounds) - if openCV 3
+    #box = np.int0(box)
+    #cv2.drawContours(debugImage,[box],0,(0,0,255),1)
+    #cv2.imwrite('debug.bmp',debugImage)
+    #cv2.imshow('debugOutput',debugImage)
+    #cv2.waitKey(0)
+    #cv2.destroyWindow('debugOutput')
+    #print(lengthModelpix)
+    #print(widthModelpix)
+    #print(lengthScaleFactor)
+    #print(widthScaleFactor)
+
+    return lengthScaleFactor,widthScaleFactor
+
+def contourHitsEdge(index,contourlist,image):
+    # returns a 1 or 0 that signifies that the given contour specified
+    # by contourlist[index] hits the maximums (edges) of the image
+    #
+    # inputs 
+    #
+    # outputs
+
+    # get image dimensions and have these be "markers"
+    left_x = 0
+    right_x = image.shape[1]-1
+    top_y = 0
+    bottom_y = image.shape[0]-1
+    # iterate over outline and check if any straight line edges hit the edge
+    edgeConflicts = 0
+
+    for each_pixel in contourlist[index][each_pixel]:
+
+        x = each_pixel[0]
+        y = each_pixel[1]
+        if x == left_x or x == right_x:
+            edgeConflicts+=1
+        if y == top_y or y == bottom_y:
+            edgeConflicts+=1
+
+    if edgeConflicts >= 20:
+        # assume we must be cutting something off from the image
+        return 1
+    return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ samain.py
@@ -0,0 +1,278 @@
+# samain.py
+#
+# author:
+# Kevin Kreher
+# KMK279@cornell.edu
+#
+# compatibility python version 2.7.10
+
+from salib import *
+import numpy as np
+import cv
+import cv2
+import math
+import csv
+import string
+
+# note: pre-process for color with Picasa
+
+# debug mode?
+debugMode = input('debug mode? (1 = yes, 0 = no): ')
+
+# remove lens distortion from all top images
+# using the asymmetrical circle pattern
+#lensCorrectImages(6,7)
+# end distort remove
+
+# calibrate pixel size to centimeters
+# we don't repeat this for each image
+bgModelLength = 4.5 # change this - in cm
+bgModelWidth = 3 # change this - in cm
+fileName = 'calibration/scale02.bmp'
+imageScale = cv2.imread(fileName,0)
+lengthScaleFactor, widthScaleFactor = pixelSizeCalibrate(imageScale,150,bgModelLength,bgModelWidth)
+# end size calibrate
+
+# open the files and process each one
+if(debugMode):
+	print('make sure there is a SeedImages_debug directory with the inteded images')
+	workingDir = 'SeedImages_debug'
+else:
+	workingDir = input('Directory name (ex. test12801/SeedImages)?: ')
+
+csvfile = open(workingDir + '_processed.csv', 'w')
+fieldnames = ['number','file path','length (cm)','width (cm)','area (pixels)',
+				'color value (R)','color value (G)','color value (B)',
+				'height (pixels)','volume (pixels)','angle (degrees)']
+writerObj = csv.DictWriter(csvfile, fieldnames=fieldnames)
+writerObj.writeheader()
+# BEGIN LOOP
+x = 0
+for fileName in glob.glob(workingDir + '/TopImage*'):
+
+	# navigate to files and open
+	imageColor = cv2.imread(fileName,1) # three channel image (B,G,R)
+	imageBW = cv2.imread(fileName,0) # BW
+	# end navigate
+
+	# debug
+	#cv2.imshow(str(fileName),imageColor)
+	#cv2.waitKey(0)
+	#cv2.destroyWindow(str(fileName))
+
+	# crop image
+	imageCroppedBW = imageBW[150:950,150:1450] # NOTE: its image[y: y + h, x: x + w]
+	imageCroppedColor = imageColor[150:950,150:1450]
+	# end crop image
+
+	# calculate length, width, area, and debug variables
+	threshTopVal = 125
+	topImgVariables = findLengthWidth(imageCroppedBW,threshTopVal)
+	topLength = topImgVariables['length']
+	topWidth = topImgVariables['width']
+	topRotateAngle = topImgVariables['angle']
+	topCenterPoint = topImgVariables['center']
+	topArea = topImgVariables['area']
+	topLargestIndex = topImgVariables['largestIndex'] 
+	topContours = topImgVariables['contours']
+	# end top calculations
+
+	# debug
+	#cv2.imshow(str(fileName),imageCroppedBW)
+	#cv2.waitKey(0)
+	#cv2.destroyWindow(str(fileName))
+	if(debugMode):
+		debugThreshTop = imageThreshold(imageCroppedBW,threshTopVal)
+		cv2.imshow(str(fileName),debugThreshTop)
+		cv2.waitKey(0)
+		cv2.destroyWindow(str(fileName))
+		debugContourTop = imageCroppedColor
+		cv2.drawContours(debugContourTop,topContours,topLargestIndex,(0,0,255),3)
+		cv2.imshow(str(fileName),debugContourTop)
+		cv2.waitKey(0)
+		cv2.destroyWindow(str(fileName))
+
+	# rotate the seed image so its length is along the x-axis
+	if topRotateAngle != 0:
+		imageCroppedBWRotated = rotateImage(imageCroppedBW,topRotateAngle,topCenterPoint)
+		# useful for complex volume finding alogirthm
+	# end rotate
+
+	# find SIDE IMAGE height, volume
+	# navigate to files and open
+	fileName_Side = string.replace(fileName,'TopImage','SideImage')
+	if (fileName_Side[-7:-4] == '000'):
+		fileName_Side = string.replace(fileName,'SideImage','Side')
+		# errors propogate... a lazy programmer made this necessary
+
+	imageColor_Side = cv2.imread(fileName_Side,1) # three channel image (B,G,R)
+	imageBW_Side = cv2.imread(fileName_Side,0)
+	# end navigate
+
+	# crop image
+	imageCroppedBW_Side = imageBW_Side[390:940,900:970] # NOTE: its image[y: y + h, x: x + w]
+	imageCroppedColor_Side = imageColor_Side[390:940,900:970]
+	# end crop image
+
+	# calculate length, width (height because side), area, and debug variables
+	threshSideVal = 125
+	sideImgVariables = findLengthWidth(imageCroppedBW_Side,threshSideVal)
+	sideLength = sideImgVariables['length']
+	sideWidth = sideImgVariables['width']
+	sideRotateAngle = sideImgVariables['angle']
+	sideCenterPoint = sideImgVariables['center']
+	sideArea = sideImgVariables['area']
+	sideLargestIndex = sideImgVariables['largestIndex'] 
+	sideContours = sideImgVariables['contours']
+	# end side calculations
+
+	# debug
+	if(debugMode):
+		debugThreshSide = imageThreshold(imageCroppedBW_Side,threshSideVal)
+		cv2.imshow(str(fileName),debugThreshSide)
+		cv2.waitKey(0)
+		cv2.destroyWindow(str(fileName))
+		debugContourSide = imageCroppedColor_Side
+		cv2.drawContours(debugContourSide,sideContours,sideLargestIndex,(0,0,255),3)
+		cv2.imshow(str(fileName),debugContourSide)
+		cv2.waitKey(0)
+		cv2.destroyWindow(str(fileName))
+
+	# calculate volume
+	#
+	# we do this by multiplying the top area times the height
+	#
+	# this may be made into a more complex function call
+	# in the future, but for now it is simple because 
+	# our height is uncalibrated
+	#	(by using as many height+width
+	#	combinations as possible and know seed to side image
+	#	angle. Make the measurements arguements to an ellipse
+	#	and use the ellipses to fill out the seed along its length
+	#	summing along the way to find total value)
+	volume = topArea*sideWidth
+	# end geometry calculation
+
+	# find average color value
+	# uses top images only because color correction was performed
+	# goes through every pixel in array and find average value of each channel
+	# !!!! note values are B G R !!!!
+
+	# Initialize empty list
+	lst_intensities = []
+	# Create a mask image that contains the contour filled in
+	cimg = np.zeros_like(imageCroppedBW)
+	cv2.drawContours(cimg,topContours,topLargestIndex,255,-1)
+
+	# debug
+	#cv2.imshow('debugOutput_mask',cimg)
+	#cv2.waitKey(0)
+	#cv2.destroyWindow('debugOutput_mask')
+
+	# Access the image pixels and create a 1D numpy array then add to list
+	pts = np.where(cimg == 255)
+	# (array([305, 305, 305, ..., 426, 426, 426]), array([507, 508, 509, ..., 707, 711, 712]))
+	i = 0
+	while i < len(pts[0]):
+		lst_intensities.append(imageCroppedColor[pts[0][i], pts[1][i]])
+		i += 1
+
+	blue = 0
+	green = 0
+	red = 0
+	i = 0
+	while i < len(lst_intensities):
+		blue += lst_intensities[i][0]
+		green += lst_intensities[i][1]
+		red += lst_intensities[i][2]
+		i += 1
+	blueAverage = blue/len(lst_intensities)
+	greenAverage = green/len(lst_intensities)
+	redAverage = red/len(lst_intensities)
+	# end color find
+
+	# scale from pixels to cm
+	lengthcm = topLength*lengthScaleFactor
+	widthcm = topWidth*widthScaleFactor
+	# end scale
+
+	# check for exceptions
+	# case 1 - contour_conflicts_edge
+	topLargestIndex
+	topContours
+	sideLargestIndex
+	sideContours
+
+	# case 2 - area_too_small
+
+	# case 3 - area_too_large
+
+	# end check for exceptions
+
+	# create debug output
+	debugImage = imageCroppedColor
+	# draw contour trace
+	cv2.drawContours(debugImage,topContours,topLargestIndex,(0,0,255),1)
+	# draw rectangle
+	# create top image rectangle (rotated) containing seed, and find midpoints
+	bounds = cv2.minAreaRect(topContours[topLargestIndex])
+	# box used for finding midpoints and drawing
+	box = cv2.cv.BoxPoints(bounds) # note cv2.cv.BoxPoints(rect) will be removed in openCV 3
+	#cv2.boxPoints(bounds) - if openCV 3
+	box = np.int0(box)
+	cv2.drawContours(debugImage,[box],0,(0,0,255),1)
+	# draw line trace
+	#cv2.line(debugImage,B,D,(0,0,255),1)
+	#cv2.line(debugImage,A,C,(0,0,255),1)
+	#cv2.circle(debugImage,A,2,(0,255,0),-1)
+	#cv2.putText(debugImage,'A',A,cv2.FONT_HERSHEY_SIMPLEX,.5,(0,255,0))
+	#cv2.circle(debugImage,B,2,(0,255,0),-1)
+	#cv2.putText(debugImage,'B',B,cv2.FONT_HERSHEY_SIMPLEX,.5,(0,255,0))
+	#cv2.circle(debugImage,C,2,(0,255,0),-1)
+	#cv2.putText(debugImage,'C',C,cv2.FONT_HERSHEY_SIMPLEX,.5,(0,255,0))
+	#cv2.circle(debugImage,D,2,(0,255,0),-1)
+	#cv2.putText(debugImage,'D',D,cv2.FONT_HERSHEY_SIMPLEX,.5,(0,255,0))
+	cv2.circle(debugImage,topCenterPoint,2,(0,255,0),-1)
+	cv2.putText(debugImage,'ctr',topCenterPoint,cv2.FONT_HERSHEY_SIMPLEX,.5,(0,255,0))
+
+	# add text
+	lengthText = 'length (cm) = ' + str(lengthcm)
+	widthText = 'width (cm) = ' + str(widthcm)
+	areaText = 'area (pixels) = ' + str(topArea)
+	colorValueText = 'color value (R G B) = (' + str(redAverage) + "," + str(greenAverage) + "," + str(blueAverage) + ")"
+	heightText = 'height (pixels) = ' + str(sideWidth)
+	volumeText = 'volume (pixels) = ' + str(volume)
+	angleText = 'angle (degrees) = ' + str(topRotateAngle)
+	cv2.putText(debugImage,fileName,(10,20),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,lengthText,(10,40),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,widthText,(10,60),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,areaText,(10,80),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,colorValueText,(10,100),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,heightText,(10,120),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,volumeText,(10,140),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+	cv2.putText(debugImage,angleText,(10,160),cv2.FONT_HERSHEY_SIMPLEX,.5,(0,0,255))
+
+	if(debugMode):
+		cv2.imshow(str(fileName),debugImage)
+		cv2.waitKey(0)
+		cv2.destroyWindow(str(fileName))
+
+	writerObj.writerow({'number':str(x),'file path':fileName,
+						'length (cm)':str(lengthcm),'width (cm)':str(widthcm),
+						'area (pixels)':str(topArea),'color value (R)':str(redAverage),
+						'color value (G)':str(greenAverage),'color value (B)':str(blueAverage),
+						'height (pixels)':str(sideWidth),'volume (pixels)':str(volume),
+						'angle (degrees)':str(topRotateAngle)})
+	print('processed: ' + fileName + '  [' + str(x) + ']')
+	x+=1
+csvfile.close()
+# !!!!!END LOOP!!!!!
+print('done: data saved in ' + workingDir + '_processed.csv')
+
+# write debug output and show image
+#cv2.imwrite('debug.bmp',debugImage)
+
+
+
+
+

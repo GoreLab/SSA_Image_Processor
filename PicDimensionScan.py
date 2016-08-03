2947205880bbeb5762508b6ccb3c36c638a2b769
CRESBV <quadraphytes@gmail.com>
2016-08-03 10:14:28 -0400

    Adding of PicDimensionScan
    This is was previously PicAverageDim. It's intension is the be able to
    scan a directory and either (based on user input) return average pic
    dimensions or place any image over a certain width in a different
    folder. Generally used to prep for manually cropping side images that
    did not crop correctly.

---
+++ PicDimensionScan.py
@@ -0,0 +1,42 @@
+__author__ = 'Edward'
+# Program made in response to many of the images from ImageCleaner not being cropped correctly. Has two functions
+# getting the average dimensions of images in a directory and placing all images over a set value pixels
+import cv2
+import os, sys
+import shutil
+# Asks user for the directory containing the images and what process should be run
+dirPath = raw_input("input path of directory with images in it: ")
+programToRun = raw_input("Which program to run average (average) or flag (find ones which are too large)?: ")
+if programToRun == "flag":
+    maxWidth = input("please enter in the value at which images will be flagged (550 advised)(integer): ")
+widthTotal = 0
+heightTotal = 0
+count = 0
+if programToRun != "flag" and programToRun != "average":
+    sys.exit("please put in average or flag (case counts!) for what program to run!")
+else:
+    count = 0
+    for file in os.listdir(dirPath):
+        if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") and programToRun == "average":
+            image = cv2.imread(dirPath + "\\" + file)
+            X = (len(image))
+            Y = (len(image[0,]))
+            widthTotal += X
+            heightTotal += Y
+
+            count += 1
+        if file.endswith(".jpg") or file.endswith(".png") and programToRun == "flag":
+            image = cv2.imread(dirPath + "\\" + file)
+            X = (len(image))
+            Y = (len(image[0,]))
+            if Y >= 550:
+                if not os.path.exists(dirPath + "\\FlaggedImages"):
+                    os.makedirs(dirPath + "\\FlaggedImages")
+                shutil.move(dirPath + "\\" + file, dirPath + "\\FlaggedImages")
+                print(dirPath + "\\" + file)
+                count += 1
+if programToRun == "average":
+    averageHeight = widthTotal / count
+    averageWidth = heightTotal / count
+    print("average height", averageHeight)
+    print("average width", averageWidth)

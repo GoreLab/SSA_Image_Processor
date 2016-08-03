a6a1692c032f2fa5fac1f4011d22a5e5757f7f91
CRESBV <quadraphytes@gmail.com>
2016-08-03 10:31:16 -0400

    Comment and light code clean up
    Cleaned up PicDimensionScan and commented it

--- Edward Programs/PicDimensionScan.py
+++ Edward Programs/PicDimensionScan.py
@@ -7,34 +7,38 @@ import shutil
 # Asks user for the directory containing the images and what process should be run
 dirPath = raw_input("input path of directory with images in it: ")
 programToRun = raw_input("Which program to run average (average) or flag (find ones which are too large)?: ")
+# If "flag" has been choosen as the function to run then the user is prompted to input the max width
 if programToRun == "flag":
     maxWidth = input("please enter in the value at which images will be flagged (550 advised)(integer): ")
+# Variables generally used in getting average
 widthTotal = 0
 heightTotal = 0
-count = 0
+# If the user inputed something other than "flag" or "average" an error is thrown
 if programToRun != "flag" and programToRun != "average":
     sys.exit("please put in average or flag (case counts!) for what program to run!")
+# Main functions of program
 else:
     count = 0
     for file in os.listdir(dirPath):
+        # If the function chosen was "average"
         if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg") and programToRun == "average":
             image = cv2.imread(dirPath + "\\" + file)
             X = (len(image))
             Y = (len(image[0,]))
             widthTotal += X
             heightTotal += Y
-
             count += 1
+        # If the function chosen was "flag"
         if file.endswith(".jpg") or file.endswith(".png") and programToRun == "flag":
             image = cv2.imread(dirPath + "\\" + file)
             X = (len(image))
             Y = (len(image[0,]))
-            if Y >= 550:
+            if Y >= maxWidth:
                 if not os.path.exists(dirPath + "\\FlaggedImages"):
                     os.makedirs(dirPath + "\\FlaggedImages")
                 shutil.move(dirPath + "\\" + file, dirPath + "\\FlaggedImages")
                 print(dirPath + "\\" + file)
-                count += 1
+# Prints results if the "average" was run
 if programToRun == "average":
     averageHeight = widthTotal / count
     averageWidth = heightTotal / count

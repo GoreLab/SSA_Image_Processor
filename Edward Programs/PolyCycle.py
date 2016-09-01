c8644737d17a393432a7a1b12b58af82d9571f34
CRESBV <quadraphytes@gmail.com>
2016-09-01 12:35:14 -0400

    Stupid Fix
    Uploaded old code this is the updated code

--- Edward Programs/PolyCycle.py
+++ Edward Programs/PolyCycle.py
@@ -5,13 +5,13 @@ import os
 
 dirPath = raw_input("please enter in the path to the folder of tifs: ")
 for file in os.listdir(dirPath):
-    if file.endswith(".tif"):
-        print("on file: " + file)
-        filePath = dirPath + "\\" + file
-        tif = gdal.open(filePath)
-        band = tif.GetBandRaster(1)
-        shpFile = file + "out.shp"
-        shpDriver = ogr.GetDriverByName("ESRI Shapefile")
-        outDataSource = shpDriver.CreateDataSource(shpFile)
-        outLayer = outDataSource.CreateLayer(shpFile, geom_type=ogr.wkbLineString)
-        gdal.Polygonize(band, None, outLayer, 1)
+	if file.endswith(".tif"):
+		print("on file: " + file)
+		filePath = dirPath + "\\" + file
+		tif = gdal.Open(filePath)
+		band = tif.GetRasterBand(1)
+		shpFile = filePath[:-4] + ".shp"
+		shpDriver = ogr.GetDriverByName("ESRI Shapefile")
+		outDataSource = shpDriver.CreateDataSource(shpFile)
+		outLayer = outDataSource.CreateLayer(shpFile, geom_type=ogr.wkbLineString )
+		gdal.Polygonize(band, None, outLayer, 1, [], callback=None)
